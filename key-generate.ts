8d2b350cad502e333218d8da79d12f5ac0a2fef9
ong-ar <reclusis@gmail.com>
2021-10-06 10:44:37 +0900

    uninstall unused packages

--- key-generate.ts
+++ key-generate.ts
@@ -1,7 +1,6 @@
 import * as bip39 from "bip39";
 import * as bip32 from "bip32";
 import { bech32 } from "bech32";
-import sha256 from "crypto-js/sha256";
 
 const MNEMONIC =
   "hub fatigue black gift believe final fine patrol unaware frozen business buyer educate leave bike festival grief much barrel retreat arrange govern exotic flee";
@@ -20,22 +19,15 @@ function main() {
 
   const node = bip32.fromSeed(seed);
   console.log("node", node);
-  console.log("node.identifier", node.identifier);
 
   const child = node.derivePath(PATH);
   console.log("child", child);
-  console.log("child", child.privateKey);
-  console.log("child", child.publicKey);
 
   const words = bech32.toWords(child.identifier);
   console.log("words", words);
 
   const result = bech32.encode(PREFIX, words);
   console.log("result", result);
-
-  console.log(
-    sha256("1010110111011000110010010010111001001011001001010110001011100001")
-  );
 }
 
 main();
