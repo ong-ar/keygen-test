a110301a0841bf7ca7bad1baa9ae060af4f66f38
ong-ar <reclusis@gmail.com>
2021-12-28 11:54:36 +0900

    private key

--- key-generate.ts
+++ key-generate.ts
@@ -1,11 +1,15 @@
 import * as bip39 from "bip39";
 import * as bip32 from "bip32";
 import { bech32 } from "bech32";
+import { ECPairFactory } from "ecpair";
+import * as TinySecp256k1 from "tiny-secp256k1";
+
+const ECPair = ECPairFactory(TinySecp256k1);
 
 const MNEMONIC =
   "hub fatigue black gift believe final fine patrol unaware frozen business buyer educate leave bike festival grief much barrel retreat arrange govern exotic flee";
 
-const PATH = "m/44/118/0/0/0";
+const PATH = "m/44'/118'/0'/0/0";
 
 const PREFIX = "cosmos";
 
@@ -22,6 +26,11 @@ function main() {
 
   const child = node.derivePath(PATH);
   console.log("child", child);
+
+  const ecpair = ECPair.fromPrivateKey(child.privateKey!, {
+    compressed: false,
+  });
+  console.log("ecpair.privatekey", ecpair.privateKey!.toString("hex"));
 
   const words = bech32.toWords(child.identifier);
   console.log("words", words);
